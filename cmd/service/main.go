7016b56bcab99b9c72c585e65fdbc8725d0c6406
Artem <garribond85@gmail.com>
2020-12-30 16:13:11 +0500

    Created main.go

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,1 +1,13 @@
-package service
+package main
+
+import (
+	"fmt"
+	"hw2.1/pkg/card"
+	"hw2.1/pkg/transfer"
+)
+
+func main() {
+
+	cardSVC := transfer.NewService(&card.Service{})
+	fmt.Println(cardSVC)
+}
